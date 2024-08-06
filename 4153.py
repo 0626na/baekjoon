e070f19bac8730d1b4a0aebe8784ae16162b4637
HANSUKJIN <0626na@gmail.com>
2024-08-06 13:07:40 +0900

    백준 4153번 문제 풀이

--- 4153.py
+++ 4153.py
@@ -1,7 +1,18 @@
-'''
-백준 알고리즘 사이트 코딩테스트 연습문제 1271번 풀이코드
-'''
-nums=[]
+"""
+백준 알고리즘 사이트 코딩테스트 연습문제 4153번 풀이코드
+"""
+
+results = []
 while 1:
-    temp=list(map(str,input().split()))
-    
+    nums = list(map(int, input().split()))
+    if nums[0] == nums[1] == nums[2] == 0:
+        break
+
+    nums.sort()
+    if nums[2] ** 2 == nums[0] ** 2 + nums[1] ** 2:
+        results.append("right")
+    else:
+        results.append("wrong")
+
+for result in results:
+    print(result)
